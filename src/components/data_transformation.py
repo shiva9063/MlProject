9117771f6a00faf9398638d8bc1ab9bbe13b87b4
Shiva Kumar <neratishivakumar2222@gmail.com>
2025-06-01 14:56:48 +0530

    templates and app created

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -21,28 +21,43 @@ class DataTransformation:
     def get_data_transformer_object(self):
         ''' this function is resiponsible for the data transformation'''
         try:
-            numerical_columns=['reading_score', 'writing_score']
-            categorical_columns=['gender','race_ethnicity','parental_level_of_education','lunch','test_preparation_course']
-            num_pipline=Pipeline(
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline= Pipeline(
                 steps=[
-                    ('imputer',SimpleImputer(strategy='median')),
-                     ('scaler',StandardScaler())
-                     ]
-            )
-            cat_pipline=Pipeline(
-                steps=[
-                    ('imputer',SimpleImputer(strategy='most_frequent')),
-                    ('one_hot_encoding',OneHotEncoder()),
-                    ('scaler',StandardScaler(with_mean=False))
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
 
                 ]
             )
-            logging.info('standard scaling completed')
-            logging.info('categorical encoding completed')
-            preprocessor=ColumnTransformer([
-                ('num_pipline',num_pipline,numerical_columns),
-                ('cat_pipline',cat_pipline,categorical_columns)
-            ])
+
+            cat_pipeline=Pipeline(
+
+                steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ]
+                )
             return preprocessor
         except Exception as e:
             raise CustomException(e,sys)
@@ -57,7 +72,7 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
             target_column_name='math_score'
-            numerical_columns=['math_score', 'reading_score', 'writing_score']
+            numerical_columns=['reading_score', 'writing_score']
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -15,12 +16,19 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_models(X_train,Y_train,X_test,Y_test,models):
+def evaluate_models(X_train,Y_train,X_test,Y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,Y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,Y_train)
+            # model.fit(X_train,Y_train)
           # make predicton
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
@@ -30,4 +38,9 @@ def evaluate_models(X_train,Y_train,X_test,Y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
-        
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
